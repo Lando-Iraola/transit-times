859e6a2586c0cb214ee3171d98161e54eccef492
Lando Vargas <lando_vargas@outlook.com>
2022-04-29 21:01:19 -0400

    Fixed my indentation a bit

--- transit-data-scrapper/src/external-data-extraction/stops.js
+++ transit-data-scrapper/src/external-data-extraction/stops.js
@@ -44,93 +44,94 @@ async function getLinesByBus(bus = "321")
  */
 function extractStops(dataToProcess, bus, alterObject)
 {
-    if(!dataToProcess)
-        throw Error("Failed bus lines data extraction. No data received");
-    if(!bus)
-        throw Error("Failed bus lines data extraction. No bus specified");;
+   if(!dataToProcess)
+      throw Error("Failed bus lines data extraction. No data received");
+   if(!bus)
+      throw Error("Failed bus lines data extraction. No bus specified");
    if(!alterObject)
       throw Error("Failed bus lines data extraction. No object reference for modification given");;
 
-    bus = bus.toLowerCase();
+   bus = bus.toLowerCase();
 
-    dataToProcess.paraderos.forEach(
-        paradero =>
-        {
-            paradero.servicios.forEach(
-                servicio =>
-                {
-                    if(servicio.cod != bus)
-                        return;
-    
-                    if(!alterObject[servicio.cod])
-                    {
-                        alterObject[servicio.cod] = {};
-                        alterObject[servicio.cod].bus = servicio.cod;
-                        alterObject[servicio.cod].lines = {};
-                    }
-    
-                    if(!alterObject[servicio.cod].lines[servicio.destino] && servicio.destino.toLowerCase() !== "fin de recorrido")
-                    {
-                        alterObject[servicio.cod].lines[servicio.destino] = 
-                        {
-                            destination: servicio.destino,
-                            stops: {
-                                [paradero.cod] :
-                                {
-                                    stop : paradero.cod,
-                                    stopOrder : servicio.orden,
-                                    commune: paradero.comuna,
-                                    geoLocation: paradero.pos,
-                                    street: paradero.name,
-                                    endOfTrip: false
-                                }
-                            }
-                        }
-                    }
-                    else if(servicio.destino.toLowerCase() !== "fin de recorrido")
-                    {
-                        alterObject[servicio.cod]
-                        .lines[servicio.destino]
-                        .stops[paradero.cod] =
-                        {
-                            stop : paradero.cod,
-                            stopOrder : servicio.orden,
-                            commune: paradero.comuna,
-                            geoLocation: paradero.pos,
-                            street: paradero.name,
-                            endOfTrip: false
-                        }
-                       
-                    }
-                    else
-                    {
-                        const entries = Object.keys(alterObject[bus].lines);
-                        for(let i = 0; i < entries.length; i++)
-                        {
-                            let communeEntry = Object.keys(alterObject[bus].lines[entries[i]].stops);
-                            communeEntry = communeEntry[communeEntry.length - 1];
-                            if(alterObject[bus].lines[entries[i]].stops[communeEntry].commune === paradero.comuna)
-                            {
-                                alterObject[servicio.cod]
-                                .lines[entries[i]]
-                                .stops[paradero.cod] =
-                                {
-                                    stop : paradero.cod,
-                                    stopOrder : servicio.orden,
-                                    commune: paradero.comuna,
-                                    geoLocation: paradero.pos,
-                                    street: paradero.name,
-                                    endOfTrip: true
-                                }
-                                break;
-                            }
-                        }
-                        
-                    }
-                }
-            )
-        }
-    )
+   dataToProcess.paraderos.forEach(
+      paradero =>
+      {
+         paradero.servicios.forEach(
+               servicio =>
+               {
+                  if(servicio.cod != bus)
+                     return;
+   
+                  if(!alterObject[servicio.cod])
+                  {
+                     alterObject[servicio.cod] = {};
+                     alterObject[servicio.cod].bus = servicio.cod;
+                     alterObject[servicio.cod].lines = {};
+                  }
+   
+                  if(!alterObject[servicio.cod].lines[servicio.destino] && servicio.destino.toLowerCase() !== "fin de recorrido")
+                  {
+                     alterObject[servicio.cod].lines[servicio.destino] = 
+                     {
+                           destination: servicio.destino,
+                           stops: 
+                           {
+                              [paradero.cod] :
+                              {
+                                 stop : paradero.cod,
+                                 stopOrder : servicio.orden,
+                                 commune: paradero.comuna,
+                                 geoLocation: paradero.pos,
+                                 street: paradero.name,
+                                 endOfTrip: false
+                              }
+                           }
+                     }
+                  }
+                  else if(servicio.destino.toLowerCase() !== "fin de recorrido")
+                  {
+                     alterObject[servicio.cod]
+                     .lines[servicio.destino]
+                     .stops[paradero.cod] =
+                     {
+                           stop : paradero.cod,
+                           stopOrder : servicio.orden,
+                           commune: paradero.comuna,
+                           geoLocation: paradero.pos,
+                           street: paradero.name,
+                           endOfTrip: false
+                     }
+                     
+                  }
+                  else
+                  {
+                     const entries = Object.keys(alterObject[bus].lines);
+                     for(let i = 0; i < entries.length; i++)
+                     {
+                           let communeEntry = Object.keys(alterObject[bus].lines[entries[i]].stops);
+                           communeEntry = communeEntry[communeEntry.length - 1];
+                           if(alterObject[bus].lines[entries[i]].stops[communeEntry].commune === paradero.comuna)
+                           {
+                              alterObject[servicio.cod]
+                              .lines[entries[i]]
+                              .stops[paradero.cod] =
+                              {
+                                 stop : paradero.cod,
+                                 stopOrder : servicio.orden,
+                                 commune: paradero.comuna,
+                                 geoLocation: paradero.pos,
+                                 street: paradero.name,
+                                 endOfTrip: true
+                              }
+                              break;
+                           }
+                     }
+                     
+                  }
+               }
+         )
+      }
+   )
 }
 
 module.exports = 
